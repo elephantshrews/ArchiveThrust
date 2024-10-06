67cb8f199334e87c246633fbe5c68ed1f4af6f6a
m.snoeken <m.snoeken@campus.tu-berlin.de>
2024-10-06 17:21:01 +0200

    Can install!

--- setup.py
+++ setup.py
@@ -3,20 +3,20 @@ import sys
 
 
 
-archive_thrust_extension = Extension(
-    name                = "ArchiveThrust",                      
+ArchiveThrust_extension = Extension(
+    name                = "src",                      
     sources             = ["ArchiveThrust/tle_download_and_parse.c",    
-             "ArchiveThrust/detect_maneuvers.c"],
+                           "ArchiveThrust/detect_maneuvers.c"],
     include_dirs        = ["include"],                   
     libraries           = ["curl", "m", "gsl", "cblas"],    
     extra_compile_args  = ["-Wall", "-Wextra", "-fPIC"], 
-    extra_link_args     = ["-shared"] if sys.platform == "win32" else ["-shared", "-Wl,-soname,ArchiveThrust.so"],  
+    extra_link_args     = ["-shared"] if sys.platform == "win32" else ["-shared", "-Wl,-soname,archive-thrust.so"],  
                                     )
 
 # Setup configuration
 setup(
     name            = "ArchiveThrust",
-    version         = "0.1.0",      
-    ext_modules     = [archive_thrust_extension],     
+    version         = "1.0.0",      
+    ext_modules     = [ArchiveThrust_extension],     
     packages        = ["ArchiveThrust"],
      )
